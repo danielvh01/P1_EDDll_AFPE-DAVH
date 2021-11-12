008785f866dac8f6011f78406c89bd9e75e0a52d
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2021-11-11 22:42:35 -0600

    login and register views

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataStructures;
 
 namespace P1_EDDll_AFPE_DAVH.Controllers
 {
     public class ChatController : Controller
     {
+        const string SessionID = "_UID";
+
 
         public static Dictionary<int, string> Rooms = new Dictionary<int, string>()
         {
@@ -18,6 +22,40 @@ namespace P1_EDDll_AFPE_DAVH.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            //Muestra la vista de inicio de sesión
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection collection)
+        {
+            //Si las credenciales son correctas iniciará sesión
+            //Si encuentra 
+            if(true)
+            {
+                //HttpContext.Session.SetString(SessionID, GetUser(collection["Username"], collection["Password"]));
+                HttpContext.Session.SetString(SessionID, collection["Username"]);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            //Muestra la vista de registro
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(IFormCollection collection)
+        {
+            //Si las credenciales son correctas y no existe el usuario crea el usuario
             return View();
         }
 
