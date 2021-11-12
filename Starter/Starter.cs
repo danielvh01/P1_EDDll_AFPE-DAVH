d3898da56c6484b3c33a42d14409142fc24341f4
danielvh01 <danielvelasquez32@gmail.com>
2021-11-12 15:18:49 -0600

    Dependencies

---
+++ Starter/Starter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace P1_EDDll_AFPE_DAVH.Starter
+{
+    public class Starter
+    {
+        public HttpClient Start()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44711");
+            return Client;
+        }
+    }
+}
